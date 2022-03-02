55dd7360a7efef60a9c63153301c57a7dd0358d3
ALejandro Binker <alemb1101@gmail.com>
2022-03-02 20:46:01 -0300

    fix(itemDetail): fixed responsive

--- src/components/itemsDetail/ItemDetail.tsx
+++ src/components/itemsDetail/ItemDetail.tsx
@@ -21,10 +21,10 @@ function ItemDetail({ item }: any) {
 
     return (
         <div className="row p-3 itemDetail">
-            <div className="col-8 text-center m-auto">
+            <div className="col-lg-8 text-center m-auto">
                 <img className="img-fluid w-75" src={item.img} alt="" />
             </div>
-            <div className="col-4 aside px-3 py-4 d-flex flex-column justify-content-around">
+            <div className="col-lg-4 aside px-3 py-4 d-flex flex-column justify-content-around">
                 <div className="details">
                     <h1 className="title">{item.title}</h1>
                     <p className="detail-precio">{`$ ${item.precio}`}</p>
@@ -33,7 +33,7 @@ function ItemDetail({ item }: any) {
                 <div className="payment-methods mt-5">
                     <p><FontAwesomeIcon icon={faCheckCircle} /><b>Garantía</b>, si este producto no cumple con tus expectativas podés devolverlo.</p>
                     <p><b>Medios de pago</b></p>
-                    <img src={credito} alt="" />
+                    <img src={credito} className="w-75" alt="pagos-tarjeta" />
                 </div>
                 <div className="buy-btn">
                     {isBuying ? <ItemCount stock={item.stock} action={onAdd} /> : <Link to="/cart"><Button variant="danger w-100">Ir al carrito</Button></Link>}
